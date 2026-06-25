a7847192b8d86fd33b6cb58910af326a45cb89a8
ezachralion <techxmanager11@outlook.com>
2026-06-25 15:50:53 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,8 @@
 console.log("This is loaded from the external JS file");
+var name = "Zacahryalion";
+let school = "NMHS";
+var fun_fact = "I'm an indie 3D Product Designer and Architect";
+
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Name: ${name}`);
