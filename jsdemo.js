315da7858ad9c08c0f99582be236b8d4b28fc47d
ezachralion <techxmanager11@outlook.com>
2026-06-30 15:52:57 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -6,3 +6,7 @@ var fun_fact = "I'm an indie 3D Product Designer and Architect";
 console.log(`Name: ${name}`);
 console.log(`School: ${school}`);
 console.log(`Name: ${name}`);
+
+document.getElementById("name").innerHTML = "<b>Name: </b><i>" + name + "</i>";
+document.getElementById("school").innerHTML = "<b>School: </b><i>" + school + "</i>";
+document.getElementById("fun_fact").innerHTML = "<b>Fun Fact: </b><i>" + fun_fact + "</i>";
