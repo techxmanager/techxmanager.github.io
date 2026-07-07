f5f8daddb3283f650000ae2578cb53d0b762006c
ezachralion <techxmanager11@outlook.com>
2026-07-07 16:23:06 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -1,7 +1,9 @@
 // 35.2271° N latitude and -80.8431° W longitude
-var charlotte = [35.2271, -80.8431]
+var charlotte = [35.2271, -80.8431];
+var cpcc = [35.2176665, -80.8314732];
 var map = L.map('map').setView(charlotte, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var cpccmarker = L.marker(cpcc).addTo(map);
